38841573633f49762093e9be9a56eb49aa51c3ba
iarocci44-byte <iarocci44@gmail.com>
2025-11-30 20:30:19 -0500

    fix subscription link

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import FrequentlyAsked from "./component/FrequentlyAsked";
 import Nav from "./component/Nav";
 import Reasons from "./component/Reasons";
-import Subscription from "./component/subscription";
+import Subscription from "./component/Subscription";
 import Trends from "./component/Trends";
 import Footer from "./component/Footer"
 //import './App.css'
